341aed0ed6b75dacdba5c7d515b4a65ec1dad421
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2022-08-24 17:56:00 +0200

    updated

--- src/components/NavigationMenu/NavigationMenu.js
+++ src/components/NavigationMenu/NavigationMenu.js
@@ -1,7 +1,58 @@
+/**
+ * Copyright (c) LuciferMorningstarDev <contact@lucifer-morningstar.dev>
+ * Copyright (c) surviv.fun <contact@surviv.fun>
+ * Copyright (C) surviv.fun team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import React from 'react';
+
+import { Link, useLocation } from 'react-router-dom';
 
 import './NavigationMenu.scss';
 
 export default function NavigationMenu() {
-    return <div className="NavigationMenu">TODO: NavigationMenu</div>;
+    const location = useLocation();
+    const [path, setPath] = React.useState(location.pathname);
+
+    React.useEffect(() => {
+        setPath(location.pathname);
+    }, [location]);
+
+    return (
+        <nav className="NavigationMenu">
+            <div className="brand"></div>
+            <ul className="nav-items">
+                <li className="nav-item">
+                    <Link to={path === '/' ? '#' : '/'} className={path === '/' ? 'nav-link active' : 'nav-link '}>
+                        Startseite
+                    </Link>
+                </li>
+
+                <li className="nav-item">
+                    <Link to={path === '/contact' ? '#' : '/contact'} className={path === '/contact' ? 'nav-link active' : 'nav-link '}>
+                        Contact
+                    </Link>
+                </li>
+
+                <li className="nav-item">
+                    <Link to={path === '/about' ? '#' : '/about'} className={path === '/about' ? 'nav-link active' : 'nav-link '}>
+                        Über Uns
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
 }
--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,6 +1,25 @@
+/**
+ * Copyright (c) LuciferMorningstarDev <contact@lucifer-morningstar.dev>
+ * Copyright (c) surviv.fun <contact@surviv.fun>
+ * Copyright (C) surviv.fun team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import React, { Component } from 'react';
 
-import { Link } from 'react-router-dom';
+import './About.scss';
 
 export default class About extends Component {
     componentDidMount() {
@@ -10,9 +29,7 @@ export default class About extends Component {
     render() {
         return (
             <div className="About">
-                About
-                <Link to="/">Homepage</Link>
-                <Link to="/contact">Kontakt</Link>
+                <h2>Hier entsteht eine neue Website</h2>
             </div>
         );
     }
