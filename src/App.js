df4e16a12c7e919336d4baa87e7bece70bf276c0
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2022-08-24 05:40:20 +0200

    clean frontend and basic project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Copyright (c) LuciferMorningstarDev <contact@lucifer-morningstar.dev>
+ * Copyright (c) surviv.fun <contact@surviv.fun>
+ * Copyright (C) surviv.fun team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+
+import './App.scss';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <>
+                <div className="App"></div>
+            </>
+        );
+    }
 }
-
-export default App;
